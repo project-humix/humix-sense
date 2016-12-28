1b0f0cae60e462a958908ac80aa1aeb341152383
Jeffrey Liu <jeffffrey@gmail.com>
2016-12-29 00:11:53 +0800

    update default config

--- sense/config.js
+++ sense/config.js
@@ -1,7 +1,5 @@
-// username and password to access itir text-to-speeach access
 
 module.exports = {
     thinkURL : 'your_think_url',
-    senseId  : 'your_sense_id',
-    log : { file: './sense.log', level: 'debug' }
+    senseId  : 'your_sense_id'
 }
